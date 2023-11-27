8266423b1be481389544450be0c4f3480473dc15
Ryan <forbesgryan@gmail.com>
2023-11-26 19:01:26 -0500

    basic implmentation

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,8 +1,4 @@
-use winapi::um::{
-    memoryapi::{OpenFileMappingW, FILE_MAP_READ, MapViewOfFile, UnmapViewOfFile},
-    // winnt::HANDLE
-};
-// use winapi::shared::minwindef::LPVOID;
+use winapi::um::memoryapi::{OpenFileMappingW, FILE_MAP_READ, MapViewOfFile, UnmapViewOfFile};
 use std::{ffi::OsStr, iter::once, collections::HashMap};
 use std::os::windows::ffi::OsStrExt;
 use std::io::{Error, ErrorKind};
@@ -81,169 +77,165 @@ pub struct Hwinfo {
     shared_memory_name: Vec<u16>,
     pub master_sensor_names: Box<Vec<String>>,
     pub master_label_user: Box<Vec<String>>,
-    pub master_readings: Box<HashMap<String,  HashMap<String, Vec<String>>>>
+    pub master_readings: Box<HashMap<String, HashMap<String, (String, [f64;4])>>>
 }
 
-#[allow(unused_variables)]
-fn init_hwinfo() -> Result<Hwinfo, Box<dyn std::error::Error>>{
-    let hwinfo_memory_size = std::mem::size_of::<HwinfoSensorsSharedMem2>();
-    // Convert the name to a wide string (UTF-16)
-    let shared_memory_name = OsStr::new(HWINFO_SENSORS_MAP_FILE_NAME2)
-        .encode_wide()
-        .chain(once(0))
-        .collect::<Vec<u16>>();
-    let shared_memory_handle = unsafe { // Open the named shared memory object for read access
-        OpenFileMappingW(
-            FILE_MAP_READ,   // Desired access
-            0,               // Inherit handle flag
-            shared_memory_name.as_ptr(),  // Name of the shared memory object
-        )
-    };
-    if shared_memory_handle.is_null() {
-        // println!("Failed to open shared memory object");
-        return Err(Box::new(Error::new(ErrorKind::NotFound,"Failed to open shared memory object")));
-    }
-    let shared_memory_view = unsafe{ // Map the shared memory into the process's address space
-        MapViewOfFile(
-            shared_memory_handle,
-            FILE_MAP_READ,
-            0,
-            0,
-            0,
-        )
-    };
-    if shared_memory_view.is_null() {
-        // println!("Failed to map view of shared memory");
-        return Err(Box::new(Error::new(ErrorKind::NotFound,"Failed to map view of shared memory")));
-    }
-    let start = shared_memory_view as *const u8;
-    let shared_memory_content = unsafe{
-        // Access the shared memory content (unsafe)
-        std::slice::from_raw_parts(start, hwinfo_memory_size)
-    };
-    let hwinfo_memory= unsafe{shared_memory_content.align_to::<HwinfoSensorsSharedMem2>().1[0]};
-    let num_sensors = hwinfo_memory.dw_num_sensor_elements;
-    let num_reading_elements = hwinfo_memory.dw_num_reading_elements;
-    let offset_sensor_section = hwinfo_memory.dw_offset_of_sensor_section;
-    let size_sensor_element = hwinfo_memory.dw_size_of_sensor_element;
-    let offset_reading_section = hwinfo_memory.dw_offset_of_reading_section;
-    let size_reading_section = hwinfo_memory.dw_size_of_reading_element;
-
-    let mut master_sensor_names: Vec<String> = Vec::new();
-    #[allow(unused_mut)]
-    let mut master_label_user: Vec<String> = Vec::new();
-    let mut master_readings: HashMap<String,  HashMap<String, Vec<String>>> = HashMap::new();
-    // let mut master_readings_array: Vec<(String, Vec<(String, Vec<String>)>)>;
-        
-    let size_u32 = std::mem::size_of::<u32>();
-    let size_utf16_string = std::mem::size_of::<[u16;HWINFO_SENSORS_STRING_LEN2]>();
-    let size_utf8_string = std::mem::size_of::<[u8;HWINFO_SENSORS_STRING_LEN2]>();
-    let size_unit_string= std::mem::size_of::<[u8;HWINFO_UNIT_STRING_LEN]>();
-    let size_f64 = std::mem::size_of::<f64>();
-    let size_sensor_reading_type = std::mem::size_of::<SensorReadingType>();
-
-    // Getting Sensor Labels
-    for dw_sensor in 0..num_sensors {
-        let offset = offset_sensor_section + (dw_sensor * size_sensor_element);
-        let ptr = unsafe {start.offset(offset as isize)};
-        let sensor_element = unsafe{std::slice::from_raw_parts(ptr, size_sensor_element as usize)};
-
-        let sensor = unsafe {&sensor_element.align_to::<HwinfoSensorsSensorElement>().1[0]};
-
-        let utf_sensor_name_user = String::from_utf8(sensor.utf_sensor_name_user.to_vec())?;
-        master_sensor_names.push(utf_sensor_name_user.clone());
-
-        let blank_reading: HashMap<String, Vec<String>> = HashMap::new();
-        master_readings.insert(utf_sensor_name_user.clone(), blank_reading);
-    }
-    
-    unsafe{ // Unmap the shared memory view when done
-        UnmapViewOfFile(shared_memory_view);
-    }
-    Ok(Hwinfo {
-        num_reading_elements,
-        offset_reading_section,
-        size_reading_section,
-        shared_memory_name,
-        master_sensor_names: Box::new(master_sensor_names),
-        master_label_user: Box::new(master_label_user),
-        master_readings: Box::new(master_readings)
-    })
-}
-
-pub fn update_hwinfo() -> Result<Hwinfo, Box<dyn std::error::Error>> {
-    let mut info = init_hwinfo()?;
-
-    let shared_memory_handle = unsafe { // Open the named shared memory object for read access
-        OpenFileMappingW(
-            FILE_MAP_READ,   // Desired access
-            0,               // Inherit handle flag
-            info.shared_memory_name.as_ptr(),  // Name of the shared memory object
-        )
-    };
-    let shared_memory_view = unsafe{ // Map the shared memory into the process's address space
-        MapViewOfFile(
-            shared_memory_handle,
-            FILE_MAP_READ,
-            0,
-            0,
-            0,
-        )
-    };
-    if shared_memory_view.is_null() {
-        println!("Failed to map view of shared memory");
-        return Err(Box::new(Error::new(ErrorKind::NotFound,"Failed to map view of shared memory")));
-    }
-    let start = shared_memory_view as *const u8;
-    // Getting Sensor Readings
-    for dw_reading in 0..info.num_reading_elements {
-        let offset = info.offset_reading_section + (dw_reading * info.size_reading_section);
-        let ptr = unsafe {start.offset(offset as isize)};
-        
-        let sensor_reading = unsafe {std::slice::from_raw_parts(ptr, info.size_reading_section as usize)};
-        if sensor_reading.len() != 460 {
-            panic!();
+impl Hwinfo {
+    pub fn new() -> Result<Hwinfo, Box<dyn std::error::Error>>{
+        let hwinfo_memory_size = std::mem::size_of::<HwinfoSensorsSharedMem2>();
+        // Convert the name to a wide string (UTF-16)
+        let shared_memory_name = OsStr::new(HWINFO_SENSORS_MAP_FILE_NAME2)
+            .encode_wide()
+            .chain(once(0))
+            .collect::<Vec<u16>>();
+        let shared_memory_handle = unsafe { // Open the named shared memory object for read access
+            OpenFileMappingW(
+                FILE_MAP_READ,   // Desired access
+                0,               // Inherit handle flag
+                shared_memory_name.as_ptr(),  // Name of the shared memory object
+            )
+        };
+        if shared_memory_handle.is_null() {
+            // println!("Failed to open shared memory object");
+            return Err(Box::new(Error::new(ErrorKind::NotFound,"Failed to open shared memory object")));
         }
-        let reading = unsafe {&sensor_reading.align_to::<HwinfoSensorsReadingElement>().1[0]};
-        let label = String::from_utf8(reading.utf_label_user.to_vec())?;
-        info.master_label_user.insert(0, label);
-
-        // Because the packed struct is unaligned
-        let value: f64 = reading.value;
-        let value_min: f64 = reading.value_min;
-        let value_max: f64 = reading.value_max;
-        let value_avg: f64 = reading.value_avg;
-
-        let mut values_list: Vec<String> = Vec::new();
-        let unit = String::from_utf8(reading.utf_unit.to_vec())?;
-        values_list.push(unit);
-        values_list.push(f64::to_string(&value));
-        values_list.push(f64::to_string(&value_min));
-        values_list.push(f64::to_string(&value_max));
-        values_list.push(f64::to_string(&value_avg));
-
-        let dw_sensor_index = reading.dw_sensor_index;
-        let current_sensor_name = &info.master_sensor_names[dw_sensor_index as usize];
-
-        if let Some(x) = info.master_readings.get_mut(current_sensor_name){
-            x.insert(
-                String::from_utf8(reading.sz_label_user.to_vec())?,
-                values_list
-            );
+        let shared_memory_view = unsafe{ // Map the shared memory into the process's address space
+            MapViewOfFile(
+                shared_memory_handle,
+                FILE_MAP_READ,
+                0,
+                0,
+                0,
+            )
+        };
+        if shared_memory_view.is_null() {
+            // println!("Failed to map view of shared memory");
+            return Err(Box::new(Error::new(ErrorKind::NotFound,"Failed to map view of shared memory")));
         }
+        let start = shared_memory_view as *const u8;
+        let shared_memory_content = unsafe{
+            // Access the shared memory content (unsafe)
+            std::slice::from_raw_parts(start, hwinfo_memory_size)
+        };
+        let hwinfo_memory= unsafe{shared_memory_content.align_to::<HwinfoSensorsSharedMem2>().1[0]};
+        let num_sensors = hwinfo_memory.dw_num_sensor_elements;
+        let num_reading_elements = hwinfo_memory.dw_num_reading_elements;
+        let offset_sensor_section = hwinfo_memory.dw_offset_of_sensor_section;
+        let size_sensor_element = hwinfo_memory.dw_size_of_sensor_element;
+        let offset_reading_section = hwinfo_memory.dw_offset_of_reading_section;
+        let size_reading_section = hwinfo_memory.dw_size_of_reading_element;
+
+        let mut master_sensor_names: Vec<String> = Vec::new();
+        // #[allow(unused_mut)]
+        let master_label_user: Vec<String> = Vec::new();
+        let mut master_readings: HashMap<String,  HashMap<String, (String, [f64;4])>> = HashMap::new();
+
+        // Getting Sensor Labels
+        for dw_sensor in 0..num_sensors {
+            let offset = offset_sensor_section + (dw_sensor * size_sensor_element);
+            let ptr = unsafe {start.offset(offset as isize)};
+            let sensor_element = unsafe{std::slice::from_raw_parts(ptr, size_sensor_element as usize)};
+            let sensor = unsafe {&sensor_element.align_to::<HwinfoSensorsSensorElement>().1[0]};
+            let utf_sensor_name_user = String::from_utf8(sensor.utf_sensor_name_user.to_vec())?.trim_matches(char::from(0)).to_string();
+            master_sensor_names.push(utf_sensor_name_user.clone());
+            let blank_reading: HashMap<String, (String, [f64;4])> = HashMap::new();
+            master_readings.insert(utf_sensor_name_user.clone(), blank_reading);
+        }
+        
+        unsafe{ // Unmap the shared memory view when done
+            UnmapViewOfFile(shared_memory_view);
+        }
+        Ok(Hwinfo {
+            num_reading_elements,
+            offset_reading_section,
+            size_reading_section,
+            shared_memory_name,
+            master_sensor_names: Box::new(master_sensor_names),
+            master_label_user: Box::new(master_label_user),
+            master_readings: Box::new(master_readings)
+        })
     }
 
-    unsafe{ // Unmap the shared memory view when done
-        UnmapViewOfFile(shared_memory_view);
-    }
+    pub fn pull(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+        // let mut hwinfo = self.new()?;
 
-    Ok(Hwinfo {
-        num_reading_elements: info.num_reading_elements,
-        offset_reading_section: info.offset_reading_section,
-        size_reading_section: info.size_reading_section,
-        shared_memory_name: info.shared_memory_name,
-        master_sensor_names: info.master_sensor_names,
-        master_label_user: info.master_label_user,
-        master_readings: info.master_readings
-    })
+        let shared_memory_handle = unsafe { // Open the named shared memory object for read access
+            OpenFileMappingW(
+                FILE_MAP_READ,   // Desired access
+                0,               // Inherit handle flag
+                self.shared_memory_name.as_ptr(),  // Name of the shared memory object
+            )
+        };
+        let shared_memory_view = unsafe{ // Map the shared memory into the process's address space
+            MapViewOfFile(
+                shared_memory_handle,
+                FILE_MAP_READ,
+                0,
+                0,
+                0,
+            )
+        };
+        if shared_memory_view.is_null() {
+            println!("Failed to map view of shared memory");
+            return Err(Box::new(Error::new(ErrorKind::NotFound,"Failed to map view of shared memory")));
+        }
+        let start = shared_memory_view as *const u8;
+        // Getting Sensor Readings
+        for dw_reading in 0..self.num_reading_elements {
+            let offset = self.offset_reading_section + (dw_reading * self.size_reading_section);
+            let ptr = unsafe {start.offset(offset as isize)};
+            
+            let sensor_reading = unsafe {std::slice::from_raw_parts(ptr, self.size_reading_section as usize)};
+            // if sensor_reading.len() != 460 {
+            //     panic!();
+            // }
+            let reading = unsafe {&sensor_reading.align_to::<HwinfoSensorsReadingElement>().1[0]};
+            let label = String::from_utf8(reading.utf_label_user.to_vec())?;
+            // self.master_label_user.insert(0, label);
+
+            // Because the packed struct is unaligned
+            let value: f64 = reading.value;
+            let value_min: f64 = reading.value_min;
+            let value_max: f64 = reading.value_max;
+            let value_avg: f64 = reading.value_avg;
+
+            let mut values_list: [f64;4] = [0.0 as f64;4];
+            let unit = String::from_utf8(reading.utf_unit.to_vec())?.trim_matches(char::from(0)).to_string();
+            // values_list.push(unit);
+            values_list[0] = value;
+            values_list[1] = value_min;
+            values_list[2] = value_max;
+            values_list[3] = value_avg;
+
+            let dw_sensor_index = reading.dw_sensor_index;
+            let current_sensor_name = &self.master_sensor_names[dw_sensor_index as usize];
+
+            if let Some(x) = self.master_readings.get_mut(current_sensor_name){
+                x.insert(
+                    label,
+                    (unit, values_list)
+                );
+            }
+        }
+
+        unsafe{ // Unmap the shared memory view when done
+            UnmapViewOfFile(shared_memory_view);
+        }
+        Ok(())
+        // Ok(Hwinfo {
+        //     num_reading_elements:   self.num_reading_elements,
+        //     offset_reading_section: self.offset_reading_section,
+        //     size_reading_section:   self.size_reading_section,
+        //     shared_memory_name:     self.shared_memory_name.clone(),
+        //     master_sensor_names:    self.master_sensor_names.clone(),
+        //     master_label_user:      self.master_label_user.clone(),
+        //     master_readings:        self.master_readings.clone()
+        // })
+    }
 }
+
+// impl Drop for Hwinfo {
+//     fn drop(&mut self){
+//         unsafe{ UnmapViewOfFile(self.shared_memory_name) };
+//     }
+// }
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,32 +1,38 @@
-use rust::update_hwinfo;
+use hwinfo_steelseries_oled::Hwinfo;
 use serde_json::json;
 use gamesense::{
     client::GameSenseClient,
     handler::screen};
-use std::{thread, time::Duration};
 
-fn main() -> Result<(), Box<dyn std::error::Error>>{
-    let mut _info = update_hwinfo().unwrap();
-    // let mut outer_key: String = String::new();
-    // let mut inner_key: String = String::new();
+fn get_inner_outer_keys(hwinfo: &Hwinfo, value: &str) -> Result<(String, String), Box<dyn std::error::Error>> {
+    let mut outer_key = String::new();
+    let mut inner_key = String::new();
+    // let _hwinfo = hwinfo.clone();
+    'outer: for (outer_k, outer_v) in hwinfo.master_readings.iter() {
+        for inner_k in outer_v.keys() {
+            if inner_k.contains(value) {
+                outer_key = outer_k.to_string();
+                inner_key = inner_k.to_string();
+                break 'outer;
+            }
+        }
+    }
+    Ok((outer_key, inner_key))
+}
 
-    // 'outer: for (k, v) in _info.master_readings.into_iter() {
-    //     for (_k, _v) in v.into_iter() {
-    //         if _k.contains("CPU (Tctl/Tdie)") {
-    //             outer_key = k;
-    //             inner_key = _k;
-    //             break 'outer;
-    //         }
-    //     }
-    // }
-    // for i in 0..10 {
-    //    _info = update_hwinfo()?; 
-    //    let (label, cpu_temp) = _info.master_readings
-    //     .get(&outer_key).unwrap()
-    //     .get_key_value(&inner_key).unwrap();
-    //     println!("{}: {}{}", label, cpu_temp[1], cpu_temp[0]);
-    //     thread::sleep(Duration::from_secs(1))
-    // }
+#[allow(unreachable_code)]
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut hwinfo = Hwinfo::new()?;
+    hwinfo.pull()?;
+    let (cpu_usage_outer_key, cpu_usage_inner_key) = get_inner_outer_keys(&hwinfo, "Total CPU Usage")?;
+    let (cpu_temp_outer_key, cpu_temp_inner_key) = get_inner_outer_keys(&hwinfo, "CPU (Tctl/Tdie)")?;
+    let (gpu_usage_outer_key, gpu_usage_inner_key) = get_inner_outer_keys(&hwinfo, "GPU Core Load")?;
+    let (gpu_temp_outer_key, gpu_temp_inner_key) = get_inner_outer_keys(&hwinfo, "GPU Temperature")?;
+    
+    let (mem_used_outer_key, mem_used_inner_key) = get_inner_outer_keys(&hwinfo, "Physical Memory Used")?;
+    let (mem_free_outer_key, mem_free_inner_key) = get_inner_outer_keys(&hwinfo, "Physical Memory Available")?;
+
+
     let mut client: GameSenseClient = GameSenseClient::new("HWINFO", "HWiNFO_Stats", "Ryan", None)?;
     let handler = screen::ScreenHandler::new("screened", "one",
         screen::ScreenDataDefinition::StaticScreenDataDefinition(screen::StaticScreenDataDefinition(
@@ -48,7 +54,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
                             }),
                             data_accessor_data: Some(screen::DataAccessorData {
                                 arg: None,
-                                context_frame_key: Some(String::from("artist"))
+                                context_frame_key: Some(String::from("line1"))
                             })
                         },
                         screen::LineData {
@@ -61,16 +67,20 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
                             }),
                             data_accessor_data: Some(screen::DataAccessorData {
                                 arg: None,
-                                context_frame_key: Some(String::from("album"))
+                                context_frame_key: Some(String::from("line2"))
                             })
                         },
                         screen::LineData {
-                            type_options: screen::LineDataType::ProgressBarData(screen::ProgressBarData {
-                                has_progress_bar: true
+                            type_options: screen::LineDataType::TextModifiersData(screen::TextModifiersData {
+                                has_text: true,
+                                prefix: None,
+                                suffix: None,
+                                bold: None,
+                                wrap: None
                             }),
                             data_accessor_data: Some(screen::DataAccessorData {
                                 arg: None,
-                                context_frame_key: None //Some(String::from("song"))
+                                context_frame_key: Some(String::from("line3"))
                             })
                         },
                     ]
@@ -81,12 +91,54 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
 
     client.bind_event("EVENT", None, None, None, None, vec![handler])?;
     client.start_heartbeat();
-    for i in 0..100 {
+    let mut i = 0;
+    loop {
+        hwinfo.pull()?; 
+        let (_, cpu_temp) = hwinfo.master_readings
+            .get(&cpu_temp_outer_key).unwrap()
+            .get_key_value(&cpu_temp_inner_key).unwrap();
+        let cpu_temp_unit  = &cpu_temp.0[0..2];
+        let cpu_temp_cur_value = &cpu_temp.1[0];
+        
+        let (_, cpu_usage) = hwinfo.master_readings
+            .get(&cpu_usage_outer_key).unwrap()
+            .get_key_value(&cpu_usage_inner_key).unwrap();
+        let cpu_usage_unit = &cpu_usage.0;
+        let cpu_usage_cur_value = &cpu_usage.1[0];
+
+        let (_, gpu_usage) = hwinfo.master_readings
+            .get(&gpu_usage_outer_key).unwrap()
+            .get_key_value(&gpu_usage_inner_key).unwrap();
+        let gpu_usage_unit = &gpu_usage.0;
+        let gpu_usage_cur_value = &gpu_usage.1[0];
+
+        let (_, gpu_temp) = hwinfo.master_readings
+            .get(&gpu_temp_outer_key).unwrap()
+            .get_key_value(&gpu_temp_inner_key).unwrap();
+        let gpu_temp_unit  = &gpu_temp.0[0..2];
+        let gpu_temp_cur_value = &gpu_temp.1[0];
+
+        let (_ , mem_used) = hwinfo.master_readings
+            .get(&mem_used_outer_key).unwrap()
+            .get_key_value(&mem_used_inner_key).unwrap();
+        let mem_used_unit = "GB";//&mem_used.0;
+        let mem_used_curr = &mem_used.1[0] / 1024.0;
+
+        let (_ , mem_free)= hwinfo.master_readings
+            .get(&mem_free_outer_key).unwrap()
+            .get_key_value(&mem_free_inner_key).unwrap();
+        let mem_free_unit = "GB"; //&mem_free.0;
+        let mem_free_curr = &mem_free.1[0] / 1024.0;
+    
         client.trigger_event_frame("EVENT", i, json!({
-            "artist": "Three Days Grace",
-            "album": "One-X",
-            "song": "Gone Forever"
+            "line1": "CPU  GPU  MEM",
+            // "line1":"12345678901234567",
+            "line2": format!("{:.1}{} {:.1}{} {:.1}{}",
+                cpu_temp_cur_value, cpu_temp_unit, gpu_temp_cur_value, gpu_temp_unit, mem_used_curr, mem_used_unit),
+            "line3": format!("{:.1}{} {:.1}{} {:.1}{}",
+                cpu_usage_cur_value, cpu_usage_unit, gpu_usage_cur_value, gpu_usage_unit, mem_free_curr, mem_free_unit),
         }))?;
+        i += 1;
     }
     client.stop_heartbeat()?;
 
