51e56ef8074995f7ae4e7880cd1a6e9ae621b5c3
Ryan <forbesgryan@gmail.com>
2023-11-30 21:48:37 -0500

    modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
+#![windows_subsystem = "windows"] // Hides the console window
 use std::num::Wrapping;
-
 use hwinfo_steelseries_oled::Hwinfo;
 use serde_json::json;
 use gamesense::{
@@ -9,7 +9,6 @@ use gamesense::{
 fn get_inner_outer_keys(hwinfo: &Hwinfo, value: &str) -> Result<(String, String), Box<dyn std::error::Error>> {
     let mut outer_key = String::new();
     let mut inner_key = String::new();
-    // let _hwinfo = hwinfo.clone();
     'outer: for (outer_k, outer_v) in hwinfo.master_readings.iter() {
         for inner_k in outer_v.keys() {
             if inner_k.contains(value) {
@@ -26,57 +25,103 @@ struct Screen {
     width: usize,
     height: usize,
 }
-
-// const NOVA_PRO: Screen = Screen{width: 128, height: 52};
+const NOVA_PRO: Screen = Screen{width: 128, height: 52};
 // const ARCTIS_PRO: Screen = Screen{width: 128, height: 48};
 
 #[allow(unreachable_code)]
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut hwinfo = Hwinfo::new()?;
-    hwinfo.pull()?;
-    let (cpu_usage_outer_key, cpu_usage_inner_key) = get_inner_outer_keys(&hwinfo, "Total CPU Usage")?;
-    let (cpu_temp_outer_key, cpu_temp_inner_key) = get_inner_outer_keys(&hwinfo, "CPU (Tctl/Tdie)")?;
-    let (gpu_usage_outer_key, gpu_usage_inner_key) = get_inner_outer_keys(&hwinfo, "GPU Core Load")?;
-    let (gpu_temp_outer_key, gpu_temp_inner_key) = get_inner_outer_keys(&hwinfo, "GPU Temperature")?;
-    
-    let (mem_used_outer_key, mem_used_inner_key) = get_inner_outer_keys(&hwinfo, "Physical Memory Used")?;
-    let (mem_free_outer_key, mem_free_inner_key) = get_inner_outer_keys(&hwinfo, "Physical Memory Available")?;
-    
-    // let screen = NOVA_PRO;
-    // let width = screen.width;
-    // let height = screen.height;
-    // let mut image: Vec<u8> = vec![0; width * height / 8];
-    // for i in 0..(width * height / 8) {
-    //     if i / width % 2 == 0 {
-    //         if i % 2 == 0 {
-    //             image[i] = 255;
-    //         }
-    //     }
-    //     else {
-    //         if i % 2 != 0 {
-    //             image[i] = 255;
-    //         }
-    //     }
-    // }
+    let mut client: GameSenseClient = match GameSenseClient::new("HWINFO", "HWiNFO_Stats", "Ryan", Some(10000)) {
+            Ok(c) => c,
+            Err(_e) => panic!("cannot connect to SteelSeries GG")
+        };
 
-    let mut client: GameSenseClient = GameSenseClient::new("HWINFO", "HWiNFO_Stats", "Ryan", None)?;
+    let hwinfo_connected = match Hwinfo::new(){
+        Ok(hwinfo) => Some(hwinfo),
+        Err(_e) => {
+            let handler = screen::ScreenHandler::new("screened", "one",
+            screen::ScreenDataDefinition::StaticScreenDataDefinition(screen::StaticScreenDataDefinition(
+                vec![screen::ScreenFrameData::MultiLineFrameData(screen::MultiLineFrameData {
+                    frame_modifiers_data: Some(screen::FrameModifiersData {
+                        length_millis: Some(10000), icon_id: None, repeats: None
+                    }),
+                    lines: vec![
+                        screen::LineData {
+                            type_options: screen::LineDataType::TextModifiersData(screen::TextModifiersData {
+                                has_text: true, prefix: None, suffix: None, bold: Some(true), wrap: None
+                            }),
+                            data_accessor_data: Some(screen::DataAccessorData {
+                                arg: None, context_frame_key: Some(String::from("error1"))
+                            })
+                        },
+                        screen::LineData {
+                            type_options: screen::LineDataType::TextModifiersData(screen::TextModifiersData {
+                                has_text: true, prefix: None, suffix: None, bold: Some(true), wrap: None
+                            }),
+                            data_accessor_data: Some(screen::DataAccessorData {
+                                arg: None, context_frame_key: Some(String::from("error2"))
+                            })
+                        }
+                    ]
+                })]
+        )));
+            client.bind_event("ERROR", None, None, None, None, vec![handler])?;
+            client.start_heartbeat();
+            client.trigger_event_frame("ERROR", 0, json!(
+                {
+                    "error1": "CAN'T CONNECT",
+                    "error2": "TO HWiNFO"
+                }))?;
+            client.stop_heartbeat()?;
+            None
+        }
+    };
+    let mut hwinfo = match hwinfo_connected {
+        Some(hw) => hw,
+        None => panic!("Cannot connect to HWiNFO")
+    };
+    hwinfo.pull()?;
+
+    let (cpu_usage_outer_key, cpu_usage_inner_key) = get_inner_outer_keys(&hwinfo, "Total CPU Usage")?;
+    let (gpu_usage_outer_key, gpu_usage_inner_key) = get_inner_outer_keys(&hwinfo, "GPU Core Load")?;
+    let (cpu_temp_outer_key, cpu_temp_inner_key)   = get_inner_outer_keys(&hwinfo, "CPU (Tctl/Tdie)")?;
+    let (gpu_temp_outer_key, gpu_temp_inner_key)   = get_inner_outer_keys(&hwinfo, "GPU Temperature")?;
+    let (mem_used_outer_key, mem_used_inner_key)   = get_inner_outer_keys(&hwinfo, "Physical Memory Used")?;
+    let (mem_free_outer_key, mem_free_inner_key)   = get_inner_outer_keys(&hwinfo, "Physical Memory Available")?;
+    
+    let screen = NOVA_PRO;
+    let width = screen.width;
+    let height = screen.height;
+    let mut image: Vec<u8> = vec![0; width * height / 8];
+    for i in 0..(width * height / 8) {
+        if i / width % 2 == 0 {
+            if i % 2 == 0 {
+                image[i] = 255;
+            }
+        }
+        else {
+            if i % 2 != 0 {
+                image[i] = 255;
+            }
+        }
+    }
+
     let handler = screen::ScreenHandler::new("screened", "one",
         screen::ScreenDataDefinition::StaticScreenDataDefinition(screen::StaticScreenDataDefinition(
             vec![
-                // screen::ScreenFrameData::ImageFrameData(screen::ImageFrameData {
-                //     has_text: false,
-                //     frame_modifiers_data: Some(screen::FrameModifiersData {
-                //         length_millis: Some(1000),
-                //         icon_id: None,
-                //         repeats: Some(screen::Repeat::Bool(false))
-                //     }),
-                //     image_data: image
-                // }),
+                screen::ScreenFrameData::ImageFrameData(screen::ImageFrameData {
+                    has_text: false,
+                    frame_modifiers_data: Some(screen::FrameModifiersData {
+                        length_millis: Some(1000),
+                        icon_id: None,
+                        repeats: Some(screen::Repeat::Bool(false))
+                    }),
+                    image_data: image
+                }),
                 screen::ScreenFrameData::MultiLineFrameData(screen::MultiLineFrameData {
                     frame_modifiers_data: Some(screen::FrameModifiersData {
                         length_millis: Some(1000),
                         icon_id: Some(screen::Icon::None),
-                        repeats: Some(screen::Repeat::Bool(true))
+                        repeats: Some(screen::Repeat::Bool(false))
                     }),
                     lines: vec![
                         screen::LineData {
