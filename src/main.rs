3416da3724854d1f49906a38827a06b6e2bd842f
Ryan <forbesgryan@gmail.com>
2023-12-10 21:06:45 -0500

    modified:   Cargo.toml 	modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,10 @@ use gamesense::{
 };
 use hwinfo_steelseries_oled::{Hwinfo, HwinfoSensorsReadingElement};
 use serde_json::json;
-use std::num::Wrapping;
-use std::io;
+use std::{num::Wrapping, io::Write};
+use console::Term;
+use ini::Ini;
+use dialoguer::Input;
 
 struct Screen {
     width: usize,
@@ -18,42 +20,43 @@ const NOVA_PRO: Screen = Screen {
 };
 // const ARCTIS_PRO: Screen = Screen{width: 128, height: 48};
 
+fn create_config(term: &Term) -> Result<Ini, anyhow::Error> {
+    term.write_line("Config not found.")?;
+    let mut conf = Ini::new();
+    term.write_line("
+    1) CPU  GPU  MEM\n
+       55°  45°  8.65G\n
+       10%  0.0% 32.0G\n
+    ")?;
+    let input: String = Input::new()
+        .with_prompt("1 / 2:")
+        .interact_text()?;
+    conf.with_section(Some("Main"))
+        .set("vertical", "false");
+
+    Ok(conf)
+}
+
 #[allow(unreachable_code)]
 fn main() -> Result<(), anyhow::Error> {
-    let mut client = match GameSenseClient::new("HWINFO", "HWiNFO_Stats", "Ryan", Some(10000)) {
-        Ok(c) => Ok(c),
-        Err(_e) => {
-            println!("cannot connect to SteelSeries GG");
-            Err(anyhow::Error::new(io::Error::new(io::ErrorKind::NotFound, "Cannot Connect to SteelSeries GG")))
-        }
-    }?;
+    let term = Term::stdout();
 
-    let hwinfo_connected = match Hwinfo::new() {
-        Ok(hwinfo) => Some(hwinfo),
-        Err(_e) => None, // let handler = page_handler(10, "error1", "error2", "", false);
-                         // client.bind_event("ERROR", None, None, None, None, vec![handler])?;
-                         // client.start_heartbeat();
-                         // client.trigger_event_frame(
-                         //     "ERROR",
-                         //     0,
-                         //     json!(
-                         //     {
-                         //         "error1": "CAN'T CONNECT",
-                         //         "error2": "TO HWiNFO"
-                         //     }),
-                         // )?;
-                         // client.stop_heartbeat()?;
-                         // None
-    };
-    let mut hwinfo = match hwinfo_connected {
-        Some(hw) => hw,
-        None => panic!("Cannot connect to HWiNFO"),
-    };
+    let mut client = connect_steelseries(&term)?;
+
+    let mut hwinfo= connect_hwinfo(&term)?;
     hwinfo.pull()?;
 
+    let mut config = match Ini::load_from_file("conf.ini") {
+        Ok(conf) => conf,
+        Err(_err) => {
+            create_config(&term)?
+        }
+    };
+
+
     let screen = NOVA_PRO;
-    let width = screen.width;
-    let height = screen.height;
+    let _width = screen.width;
+    let _height = screen.height;
 
     let page1_handler = page_handler(3, "line1", "line2", "line3", false);
     let page2_handler = page_handler(3, "line1", "line2", "line3", false);
@@ -72,40 +75,56 @@ fn main() -> Result<(), anyhow::Error> {
         let sensor_gpu_temp = hwinfo.find("GPU Temperature").unwrap();
         let sensor_mem_used = hwinfo.find("Physical Memory Used").unwrap();
         let sensor_mem_free = hwinfo.find("Physical Memory Available").unwrap();
+        let sensor_mem_load = hwinfo.find("Physical Memory Load").unwrap();
 
         let cpu_temp_cur_value = sensor_cpu_temp.value;
-        let cpu_temp_unit = "°"; //String::from_utf8(sensor_cpu_temp.utf_unit.to_vec())?;
         let cpu_usage_cur_value = sensor_cpu_usage.value;
-        let cpu_usage_unit = "%"; //String::from_utf8(sensor_cpu_usage.utf_unit.to_vec())?;
+        let temp_unit = "°"; //String::from_utf8(sensor_cpu_temp.utf_unit.to_vec())?;
+        let usage_unit = "%"; //String::from_utf8(sensor_cpu_usage.utf_unit.to_vec())?;
 
         let gpu_temp_cur_value = sensor_gpu_temp.value;
-        let gpu_temp_unit = "°"; //String::from_utf8(sensor_gpu_temp.utf_unit.to_vec())?;
         let gpu_usage_cur_value = sensor_gpu_usage.value;
-        let gpu_usage_unit = "%"; //String::from_utf8(sensor_gpu_usage.utf_unit.to_vec())?;
 
-        let mem_unit = "GB";
+        let mem_unit = "g";
         let mem_used = sensor_mem_used.value / 1024.0;
         let mem_free = sensor_mem_free.value / 1024.0;
+        let mem_load = sensor_mem_load.value;
 
         let line1_spaces = "  ";
         let line2_spaces = "  ";
 
-        value = json!({
-            "line1": "CPU    GPU    MEM",
-            "line2": format!("{:.1}{}{}{:.1}{}{}{:.1}{}",
-                cpu_temp_cur_value, cpu_temp_unit,
-                line1_spaces,
-                gpu_temp_cur_value, gpu_temp_unit,
-                line1_spaces,
-                mem_used, mem_unit.to_lowercase()),
-            "line3": format!("{:.1}{}{}{:02.1}{}{}{:.1}{}",
-                cpu_usage_cur_value, cpu_usage_unit,
-                line2_spaces,
-                gpu_usage_cur_value, gpu_usage_unit,
-                line2_spaces,
-                mem_free, mem_unit.to_lowercase()),
-        });
-
+        let vertical: bool = false;
+        if vertical {
+            value = json!({
+                "line1": "CPU    GPU    MEM",
+                "line2": format!("{:.1}{}{}{:.1}{}{}{:.1}{}",
+                    cpu_temp_cur_value, temp_unit,
+                    line1_spaces,
+                    gpu_temp_cur_value, temp_unit,
+                    line1_spaces,
+                    mem_used, mem_unit),
+                "line3": format!("{:.1}{}{}{:02.1}{}{}{:.1}{}",
+                    cpu_usage_cur_value, usage_unit,
+                    line2_spaces,
+                    gpu_usage_cur_value, usage_unit,
+                    line2_spaces,
+                    mem_free, mem_unit),
+            });
+        } else {
+            value = json!({
+                "line1": format!("CPU  {:.1}{}  {:.1}{}",
+                    cpu_temp_cur_value, temp_unit,
+                    cpu_usage_cur_value, usage_unit),
+                "line2": format!("GPU  {:.1}{}  {:.1}{}",
+                    gpu_temp_cur_value, temp_unit,
+                    gpu_usage_cur_value, usage_unit),
+                "line3": format!("MEM {:.1}{} {:.1}{}",
+                    mem_used, mem_unit,
+                    mem_load, usage_unit,
+                    // mem_free, mem_unit.to_lowercase()
+                ),
+            });
+        }
         // if i.0 % 3 == 0 {
         //     client.trigger_event_frame("EVENT2", i.0, json!({
         //         "line1":"Hello!",
@@ -123,6 +142,56 @@ fn main() -> Result<(), anyhow::Error> {
     client.stop_heartbeat()?;
 
     Ok(())
+}
+
+
+fn connect_hwinfo(term: &Term) -> Result<Hwinfo, anyhow::Error>{
+    match Hwinfo::new() {
+        Ok(hwinfo) => {
+            term.write_line("Connected to HWiNFO")?;
+            Ok(hwinfo)
+        },
+        Err(_err) => {
+            // println!("Can't connect to HWiNFO. Trying again in 1 second.");
+            for i in (1..=3).rev() {
+                term.write_line(format!("Can't connect to HWiNFO. Trying again in {} second.", i).as_str())?;
+                std::thread::sleep(std::time::Duration::from_secs(1));
+                term.clear_line()?;
+            }
+            connect_hwinfo(term)
+        }
+    }
+}
+
+fn connect_steelseries(term: &Term) -> Result<GameSenseClient, anyhow::Error> {
+    match GameSenseClient::new("HWINFO", "HWiNFO_Stats", "Ryan", Some(10000)) {
+        Ok(c) => {
+            term.write_line("Connected to SteelSeries GG")?;
+            Ok(c)
+        },
+        Err(_e) => {
+            for i in (1..=3).rev() {
+                term.write_line(format!("Can't connect to SteelSeries GG. Trying again in {} second.", i).as_str())?;
+                std::thread::sleep(std::time::Duration::from_secs(1));
+                term.clear_line()?;
+            }
+            connect_steelseries(term)
+        }
+    }
+}
+
+
+fn hide_console_window() {
+    use std::ptr;
+    use winapi::um::wincon::GetConsoleWindow;
+    use winapi::um::winuser::{ShowWindow, SW_HIDE};
+    let window = unsafe { GetConsoleWindow() };
+    // https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-showwindow
+    if window != ptr::null_mut() {
+        unsafe {
+            ShowWindow(window, SW_HIDE);
+        }
+    }
 }
 
 fn page_handler(
@@ -194,17 +263,4 @@ fn page_handler(
             )]),
         ),
     )
-}
-
-fn hide_console_window() {
-    use std::ptr;
-    use winapi::um::wincon::GetConsoleWindow;
-    use winapi::um::winuser::{ShowWindow, SW_HIDE};
-    let window = unsafe { GetConsoleWindow() };
-    // https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-showwindow
-    if window != ptr::null_mut() {
-        unsafe {
-            ShowWindow(window, SW_HIDE);
-        }
-    }
 }
